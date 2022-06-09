2e1cc4d5454aeee9a0b97b4748382a7cb0206029
jacoposergio <djago32@gmail.com>
2022-06-09 17:34:36 +0200

    snack 3 finito

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -2,11 +2,25 @@
 // Il programma stampa la somma di tutti i numeri inseriti.
 
 
+// lascio l'array vuoto e ci pusho dentro il prompt (con la richiesta del numero)
+// per 10 volte grazie al ciclo for
+
 let numbers = [];
 for(let i = 0; i < 10; i++)
-   numbers.push(prompt("Inserisci un numero"));
+   numbers.push( parseInt( prompt("Inserisci un numero")));
 
    console.log(numbers);
+  
+//    ora che ho riempito l'array con i numeri dell'utente, ripeto il ciclo for
+// e sommo tutti i numeri alla nuova variabile ( che è uguale a 0) 
+   let sumNumber = 0
+   for(let i = 0; i < numbers.length; i++){
+       console.log( parseInt(numbers[i]));
+   
+       sumNumber += ( parseInt(numbers[i]));
+   }
+   
+//    stampo in pagina il risultato 
 
-   sumNumber +=  numbers.push(prompt("Inserisci un numero"));
    console.log(sumNumber);
+   document.getElementById('user-message').innerHTML += 'La somma dei numeri è '+ sumNumber  ; 
